ec43fe3b6e021e90d3d75e8caa36615bfaeba06f
hopnh <haohopnguyen@gmail.com>
2026-03-07 23:40:13 +0700

    added macro hash

---
+++ macros/utils/hash.sql
@@ -0,0 +1,15 @@
+{%- macro hash_column(columns_list, source_name) -%}
+    {%- set hex_columns = [] -%}
+    {%- set len = columns_list | length -%}
+    {%- for column in columns_list -%}
+        {%- if loop.index != len -%}
+            {%- set column_string = "COALESCE(TRIM(CAST(" ~ column ~ " AS string)), '') || " -%}
+        {%- else -%}
+            {%- set column_string = "COALESCE(TRIM(CAST(" ~ column ~ " AS string)), '') " -%}
+        {%- endif -%}
+        {%- do hex_columns.append(column_string) -%}
+    {%- endfor -%}
+
+    {%- set str = "'" ~ source_name ~ "__' || " ~ hex_columns | join('') -%}
+    md5({{ str }})
+{%- endmacro -%}
