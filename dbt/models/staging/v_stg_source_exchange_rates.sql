bf5731bc194a1d1737337fd8227e1c16f8d4cb69
hopnh <haohopnguyen@gmail.com>
2026-03-30 22:42:59 +0700

    update staging

--- dbt/models/staging/v_stg_source_exchange_rates.sql
+++ dbt/models/staging/v_stg_source_exchange_rates.sql
@@ -7,18 +7,9 @@
 {% set source_date_col = 'data_date' %}
 {% set business_key_cols = ['data_date', 'currency'] %}
 
-SELECT
-    {{ hash_column(business_key_cols, source_name) }} as hashkey,
-    data_date,
-    currency,
-    rate,
-
-    '{{ var("target_date") }}' as source_date,
-    cast(current_timestamp as timestamp) as load_timestamp,
-    '{{ source_name }}' as source_system
-
-FROM {{ source(source_name, source_table) }}
-
-{% if execute %}
-WHERE {{ to_yyyymmdd_str(source_date_col) }} = '{{ var("target_date") }}'
-{% endif %}
+{{ staging(
+    source_name=source_name,
+    source_table=source_table,
+    source_date_col=source_date_col,
+    business_key_cols=business_key_cols
+) }}
