c13a3636edefad3f897fdf2376fb0ce5dc3fb048
hopnh <haohopnguyen@gmail.com>
2026-04-21 22:57:10 +0700

    update fact_geo_daily

--- dbt/models/marts/fact_geo_daily.sql
+++ dbt/models/marts/fact_geo_daily.sql
@@ -6,9 +6,9 @@ select
     cast(to_char(pit.order_purchase_timestamp::date, 'YYYYMMDD') as text) as date_key,
     pit.order_purchase_timestamp::date as full_date,
     count(*) as total_orders,
-    sum(coalesce(total_payment_value, 0)) as total_revenue,
-    avg(case when is_delivered_late then 1.0 else 0.0 end) as late_delivery_ratio,
-    avg(avg_review_score) as avg_review_score,
+    sum(coalesce(fo.total_payment_value, 0)) as total_revenue,
+    avg(case when fo.is_delivered_late then 1.0 else 0.0 end) as late_delivery_ratio,
+    avg(fo.avg_review_score) as avg_review_score,
     current_timestamp as mart_load_timestamp
 from {{ ref('bridge_order_current_snapshot') }} pit
 left join {{ ref('fact_orders') }} fo
